25896de92e8e790e62cf555dcf9784fbdef2b28e
An Huynh <ahuynh@emailatg.com>
2018-06-30 17:08:48 -0500

    Creating foundational solution

---
+++ AnsShop/AnsShop.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AnsShop.WebUI.MvcApplication" Language="C#" %>
---
+++ AnsShop/AnsShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AnsShop.WebUI.Startup))]
+namespace AnsShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
